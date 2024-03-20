7fb7d1d77d6801552c9321c545ec0896b5130224
shubham321-ux <shubham4260shubhu@gmail.com>
2024-03-20 05:36:09 -0700

    add modern site project

--- src/data/constants.js
+++ src/data/constants.js
@@ -131,33 +131,7 @@ export const experiences = [
 ];
 
 export const education = [
-  {
-    id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-    date: "Oct 2021 - Sep 2025",
-    grade: "8.71 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
-  },
-  {
-    id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2019 - Apr 2025",
-    grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-    degree: "ISC(XII), Science with Computer",
-  },
-  {
-    id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
-  },
+ 
 ];
 
 export const projects = [
@@ -192,6 +166,20 @@ export const projects = [
     category: "web app",
     github: "https://github.com/shubham321-ux/flipcart_homePage_clone_Project",
     webapp: "https://shubham321-ux.github.io/flipcart_homePage_clone_Project/",
+   
+  },
+   {
+    id: 0,
+    title: "Modern Animated Site",
+    date: "jan 2024 ",
+    description:
+      "modern animated wesite with scroll trigger & GSAP library amazing animation smooth scrolling web 3.0 concept ",
+    image:
+      "https://github.com/shubham321-ux/mordern-website/blob/main/modern.png?raw=true",
+    tags: ["HTML", "CSS", "javascript","GSAP","Scrolltrigger"],
+    category: "web app",
+    github: "https://github.com/shubham321-ux/mordern-website",
+    webapp: "https://shubham321-ux.github.io/mordern-website/",
    
   },
   {
